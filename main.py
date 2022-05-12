c9f220b252769bb82ac1b2c8bd390d4a116f9eeb
Leandro <lean.arbelo.dev@gmail.com>
2022-05-12 20:09:01 -0300

    Resolviendo errores de git

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import pygame, sys
+from score_file import *
 from constants import *
 from random import randint
 from floor import Floor
@@ -18,7 +19,7 @@ pygame.display.set_caption("GOSH, Cari!")
 screen = pygame.display.set_mode((SIZE, SIZE))
 
 # Font
-game_font = pygame.font.Font('Fonts/Super-Mario2.ttf', 27)
+game_font = pygame.font.Font('Fonts/Super-Mario2.ttf', 20)
 
 # Clock object
 clock = pygame.time.Clock()
@@ -49,6 +50,35 @@ enemy_img = pygame.transform.scale(enemy_img_original, ENEMY_SIZE).convert_alpha
 skull_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
 
+# Flag for change the heading direction of the player
+is_left = False
+
+# Flag for pause the game until the player press any key
+is_on = False
+
+# Game over
+game_over = False
+
+def message_game_over(msg, go_y):
+    text = game_font.render(msg, False, (10, 0, 50))
+    go_x = int(SIZE / 2)
+    go_rect = text.get_rect(center = (go_x, go_y))
+    screen.blit(text, go_rect)
+    
+game_over_time = 0
+       
+# Handling scores
+current_dir = Path.cwd()
+path = Path(current_dir)
+best_score_file = path / 'best_score.txt'
+       
+new_best_score = 0
+new_best_score_time = 0
+
+def is_new_score():
+    if seconds < new_best_score_time + 3:
+        return message_game_over('New highest score!', int(SIZE / 2))
+
 # Main
 class Main():
     def __init__(self):
@@ -57,6 +87,7 @@ class Main():
         self.cloud = Clouds()
         self.decorator = Decorator(decorator_img)
         self.player = Player(CELL_SIZE, PLAYER_FLOOR)
+        # Sounds and music
         self.music = pygame.mixer.Sound('Sounds/ghost-sounds-theremin.wav')
         self.bonus_music = pygame.mixer.Sound('Sounds/arcade-retro-run.wav')
         self.reload_sound = pygame.mixer.Sound('Sounds/recharging.wav')
@@ -65,6 +96,7 @@ class Main():
     def draw_objects(self):
         self.cloud.draw_clouds(screen, clouds_img)
         self.score.draw_score(game_font, screen)
+        self.score.draw_best_score(game_font, screen, ('BEST: ' + str(score_file_content)))
         self.floor.draw_floor(screen)
         
     def play_music(self, sound, repeat):
@@ -83,75 +115,14 @@ main = Main()
 # Musical theme
 main.play_music(main.music, -1)
 
-# Flag for change the heading direction of the player
-is_left = False
-
-# Game over
-game_over = False
-
-def message_game_over(msg):
-    text = game_font.render(msg, False, (10, 0, 50))
-    go_x = int(SIZE / 2)
-    go_y = int(SIZE / 2)
-    go_rect = text.get_rect(center = (go_x, go_y))
-    screen.blit(text, go_rect)
-       
-mejor_puntaje = 100
-
-is_on = False
-
-game_over_time = 0
-
 # Main Loop
 while True:
+    score_file_content = read_best_score(best_score_file)
     
-    # Quit event
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    key_pressed = pygame.key.get_pressed()
-    
-    # Reload position
-    if key_pressed[pygame.K_SPACE]:
-        main.reload(main.reload_sound)
-        main.score.score_text = 0
-    
-    # Assign keys for move the player
-    if key_pressed[pygame.K_LEFT]:
-        main.player.left()
-        is_left = True
-        is_on = True
+    """ if main.score.score_text > mejor_puntaje:
+        new_best_score_time = seconds
+        mejor_puntaje = main.score.score_text """
         
-    elif key_pressed[pygame.K_RIGHT]:
-        main.player.right()
-        is_left = False
-        is_on = True
-        
-    elif key_pressed[pygame.K_UP]:
-        main.player.up()
-        is_on = True
-        
-    elif key_pressed[pygame.K_DOWN]:
-        main.player.down()
-        is_on = True
-    
-    if event.type == pygame.KEYUP:
-        main.player.down()
-        is_on = True
-    
-    
-    """ FOR A SECOND PLAYER
-    if key_pressed[pygame.K_a]:
-        main.player2.left()
-    elif key_pressed[pygame.K_d]:
-        main.player2.right()
-    elif key_pressed[pygame.K_w]:
-        main.player2.up()
-    elif key_pressed[pygame.K_s]:
-        main.player2.down() """
-                
     # Bg color of the screen    
     if main.score.score_text < 200:    
         screen.fill(SKY_COLOR)
@@ -161,7 +132,7 @@ while True:
                 
     # Adding some skulls to skull_group and defining some progressive levels   
     if is_on:      
-        if len(skull_group) < 1:
+        if len(skull_group) == 0:
             if main.score.score_text < 200:
                 for number in range(0, 15):           
                     skull = Skulls(skull_img, skull_group, randint(2, 3))
@@ -220,40 +191,55 @@ while True:
                 for number in range(0, 3):           
                     enemy = Skulls(enemy_img, enemy_group, randint(3, 8))
                     enemy_group.add(enemy)
+                # if the player fly to the top, more enemies drops
+                if main.player.rect.y < (SIZE / 3):
+                    for number in range(2, 10):
+                        enemy = Skulls(enemy_img, enemy_group, randint(3, 8))
+                        enemy_group.add(enemy)
                     
     # Drawing enemies creating an instance of class Skulls
-    for enemy in enemy_group:
-        enemy.draw_skulls(screen)
-        enemy.dropping(randint(3, 8))
-        # When they hit the floor
-        if enemy.rect.y > (main.floor.floor_rect.top - CELL_SIZE):
-            enemy_group.remove(enemy)
-            main.score.score_text += 6
-        # When they collied
-        elif pygame.sprite.spritecollide(main.player, enemy_group, True):
-            enemy_group.remove(enemy)
-            main.score.score_text = 0
-            main.reload(main.game_over_sound)
-            game_over = True
-            time.sleep(1.2)
-           
+        for enemy in enemy_group:
+            enemy.draw_skulls(screen)
+            enemy.dropping(randint(3, 8))
+            # When they hit the floor
+            if enemy.rect.y > (main.floor.floor_rect.top - CELL_SIZE):
+                enemy_group.remove(enemy)
+                main.score.score_text += 6
+            # When they collied
+            elif pygame.sprite.spritecollide(main.player, enemy_group, True):
+                enemy_group.remove(enemy)
+                main.reload(main.game_over_sound)
+                game_over = True
+                game_over_time = seconds
+                print(game_over_time)
+                last_score = main.score.score_text
+                main.score.score_text = 0
+                time.sleep(1.1)
+
+    
     # Draw elements
     main.draw_objects()
     
-    # Game over text
-    if main.score.score_text < 6:
-        if game_over == True:
-            message_game_over('GAME OVER')
-            
-    if main.score.score_text > mejor_puntaje and main.score.score_text < (mejor_puntaje + 30):
-        message_game_over('New highest score!')
-    
-        
     seconds = pygame.time.get_ticks() / 1000
     #print(seconds)    
-    if seconds < 5:
-        message_game_over('BEST SCORE: ' + str(mejor_puntaje))
+    if seconds < 3:
+        message_game_over('BEST SCORE: ' + str(score_file_content), int(SIZE / 2))
      
+    
+    # Game over text
+    if seconds < game_over_time + 4:
+        if game_over == True:
+            message_game_over('GAME OVER', int(SIZE / 2 - CELL_SIZE))
+            message_game_over('Your score: ' + str(last_score), int(SIZE / 2 + CELL_SIZE))
+            if main.score.score_text > int(score_file_content):
+                change_best_score_file(score_file_content, best_score_file, new_best_score)
+                
+    # New best score
+    if main.score.score_text > int(score_file_content):
+        new_best_score = main.score.score_text
+        new_best_score_time = seconds
+        change_best_score_file(score_file_content, best_score_file, new_best_score)
+        is_new_score()
     
     # Changing the flag
     if is_left:
@@ -263,6 +249,56 @@ while True:
         
     # Decorator drawed after all other elements
     main.decorator.draw_decorator(screen)
+    
+    
+    # EVENTS
+        
+    # Quit event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    key_pressed = pygame.key.get_pressed()
+    
+    # Reload position
+    if key_pressed[pygame.K_SPACE]:
+        main.reload(main.reload_sound)
+        main.score.score_text = 0
+    
+    # Assign keys for move the player
+    if key_pressed[pygame.K_LEFT]:
+        main.player.left()
+        is_left = True
+        is_on = True
+        
+    elif key_pressed[pygame.K_RIGHT]:
+        main.player.right()
+        is_left = False
+        is_on = True
+        
+    elif key_pressed[pygame.K_UP]:
+        main.player.up()
+        is_on = True
+        
+    elif key_pressed[pygame.K_DOWN]:
+        main.player.down()
+        is_on = True
+    
+    if event.type == pygame.KEYUP:
+        main.player.down()
+        is_on = True
+    
+    
+    """ FOR A SECOND PLAYER
+    if key_pressed[pygame.K_a]:
+        main.player2.left()
+    elif key_pressed[pygame.K_d]:
+        main.player2.right()
+    elif key_pressed[pygame.K_w]:
+        main.player2.up()
+    elif key_pressed[pygame.K_s]:
+        main.player2.down() """
         
     # Updating the display surface with all the elements that were drawn in the screen.
     pygame.display.update()
