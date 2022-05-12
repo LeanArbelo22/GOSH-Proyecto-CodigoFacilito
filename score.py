54f02c302aef518caa6c24f474d3d597d186897f
Leandro <lean.arbelo.dev@gmail.com>
2022-05-12 20:17:44 -0300

    resolviendo errores al pushear al repo

--- score.py
+++ score.py
@@ -6,10 +6,17 @@ class Score():
         self.score_text = 0 # !!!
         
     def draw_score(self, game_font, screen):
-        score_surface = game_font.render(str(self.score_text), False, SCORE_COLOR)
-        score_x = int(CELL_SIZE + CELL_SIZE)
-        score_y = int(CELL_SIZE + (CELL_SIZE / 2)) 
-        score_rect = score_surface.get_rect(center = (score_x, score_y))
-        screen.blit(score_surface, score_rect)     
-        line_rect = pygame.Rect(score_rect.left - 4, score_rect.top - 2, score_rect.width + 6, score_rect.height + 4)
-        pygame.draw.rect(screen, SCORE_COLOR, line_rect, 2)
+        self.score_surface = game_font.render(str(self.score_text), False, SCORE_COLOR)
+        self.score_x = int(CELL_SIZE * 2)
+        self.score_y = int(CELL_SIZE + (CELL_SIZE / 2)) 
+        self.score_rect = self.score_surface.get_rect(center = (self.score_x, self.score_y))
+        self.line_rect = pygame.Rect(self.score_rect.left - 4, self.score_rect.top - 2, self.score_rect.width + 6, self.score_rect.height + 4)
+        pygame.draw.rect(screen, SCORE_COLOR, self.line_rect, 2)
+        screen.blit(self.score_surface, self.score_rect)     
+        
+    def draw_best_score(self, game_font, screen, best_score):
+        self.score_surface = game_font.render(str(best_score), False, SCORE_COLOR)
+        self.score_x = int(SIZE - (CELL_SIZE * 4))
+        self.score_y = int(CELL_SIZE + (CELL_SIZE / 2)) 
+        self.score_rect = self.score_surface.get_rect(center = (self.score_x, self.score_y))
+        screen.blit(self.score_surface, self.score_rect)   
